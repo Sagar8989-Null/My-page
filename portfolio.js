b4ba1c8a9b24cb8f224d699365be27f8ff8a5272
Sagar8989-Null <sagarjamadar8989@gmail.com>
2025-01-27 21:00:05 +0530

    Dark mode button added

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,22 @@ function Cvoption() {
         display=0;
     }
 }
+var dark=0;
+
+function theme()
+{
+        const root = document.documentElement;
+        if (dark==0){
+            root.style.setProperty('--color1', '#fd6f00');
+            root.style.setProperty('--color2', '#1E1E1E');
+            root.style.setProperty('--color3', 'white');
+            dark=1;
+        }
+        else
+        {
+            root.style.setProperty('--color1', '#fd6f00');
+            root.style.setProperty('--color2', 'white');
+            root.style.setProperty('--color3', '#1E1E1E');
+            dark=0;
+        }
+}
